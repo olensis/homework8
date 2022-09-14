21ef5a47d989a6be38a6ca88d948e93127f1a0cc
olensis <olensis7gmail@.com>
2022-09-14 17:25:58 +0300

    сделала два задания

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double task1 = 1.2;
+        int numbers[] = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println(Arrays.toString(numbers));
+        double[] fractional = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(fractional));
+        int[] guestion = {0, 5, 25, 40};
+        System.out.println(Arrays.toString(guestion));
     }
-    int task1= 1;
+    {
+        int task3=3;
+        int numbers1[] = new int[3];
+        numbers1[0] = 1;
+        numbers1[1] = 2;
+        numbers1[2] = 3;
+        for (int i = numbers1.length - 1; i >=0; i--) {
+            System.out.println(Arrays.toString(numbers1));
+        }
+        double[] fractional1 = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(fractional1));
+        int[] guestion1 = {0, 5, 25, 40};
+        System.out.println(Arrays.toString(guestion1));
 
-}
+
+
+
+    }
+
+
+
+
+    }
+
